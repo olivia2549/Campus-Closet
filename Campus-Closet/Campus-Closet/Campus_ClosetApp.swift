7b00333696a225d996c9ab7b91087c9aa2d531a4
olivia2549 <ologanmail@gmail.com>
2022-09-22 15:50:39 -0500

    initial commit

---
+++ Campus-Closet/Campus-Closet/Campus_ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_ClosetApp.swift
+//  Campus-Closet
+//
+//  Created by Olivia Logan on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Campus_ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Campus-Closet/Campus-Closet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Campus-Closet
+//
+//  Created by Olivia Logan on 9/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
