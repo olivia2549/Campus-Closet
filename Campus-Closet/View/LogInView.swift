8e0df4dc4199e7ec2eab467788b6a1fdee8e5c19
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2022-10-06 15:44:32 -0500

    push
    pushhhh

--- Campus-Closet/View/LogInView.swift
+++ Campus-Closet/View/LogInView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct LogInView: View {
     var body: some View {
         VStack(spacing: 0) {
+            Image("logo")
             
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         }
