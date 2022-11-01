699ccaa7f8038a8f77194e488ce6f548382043f1
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2022-11-01 13:44:49 -0500

    Update chatview

--- Campus-Closet/Views/Messages/ChatView.swift
+++ Campus-Closet/Views/Messages/ChatView.swift
@@ -7,23 +7,23 @@
 import SwiftUI
 
 struct ChatView: View {
-    //    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var imageUrl = URL(string: "https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074_1280.jpg")
     var name = "Lauren Scott"
     var body: some View {
         HStack(spacing: 0){
-            //                Styles.BackButton(presentationMode: self.presentationMode)
-            //                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true)) {
-                Image(systemName: "chevron.backward")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 20, height: 20)
-                    //.padding(20)
-                    .foregroundColor(.black)
-                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 15))
-            }
+            Styles.BackButton(presentationMode: self.presentationMode)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+//            NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true)) {
+//                Image(systemName: "chevron.backward")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: 20, height: 20)
+//                    //.padding(20)
+//                    .foregroundColor(.black)
+//                    .padding(EdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 15))
+//            }
             
             
             AsyncImage(url: imageUrl){ image in
