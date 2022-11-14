4b146dee64b1f4e2ff36b2f4cc66962749ad7166
Lauren Scott <lauren.e.scott@vanderbilt.edu>
2022-11-14 13:22:51 -0600

    Backend messaging support in view model

--- Campus-Closet/ViewModels/MessagesVM.swift
+++ Campus-Closet/ViewModels/MessagesVM.swift
@@ -13,38 +13,65 @@ import FirebaseAuth
 class MessagesVM: ObservableObject {
     @Published var users: [String] = []
     @Published private(set) var messages: [Message] = []
+    @Published var recentMessages: [String: Message] = [:]
     @Published private(set) var lastMessageID = ""
     @Published var errorMessage: String = ""
     let db = Firestore.firestore()
 
     init() {
         fetchAllUsers()
-        // getMessages()
-        getConversation()
+        getMyMessages()
+        getMyMessageHistory()
     }
     
-    func getConversation() {
+    func getMyMessages() {
         let currentId = Auth.auth().currentUser!.uid
-        let otherId = "eyG6aeR5EpO0sY46wFIaNVg9qSw2"
         
-        let sentMessages = db.collection("Messages")
-            .whereField("sender", isEqualTo: currentId)
-            .whereField("recipient", isEqualTo: otherId)
-        let receivedMessages = db.collection("Messages")
-            .whereField("sender", isEqualTo: otherId)
-            .whereField("recipient", isEqualTo: currentId)
-        
+        // Fetch all messages I have sent.
+        let sentMessages = db.collection("Messages").whereField("sender", isEqualTo: currentId)
         getMessages(query: sentMessages)
+        
+        // Fetch all messages I have received.
+        let receivedMessages = db.collection("Messages").whereField("recipient", isEqualTo: currentId)
         getMessages(query: receivedMessages)
         
         self.messages.sort {
             $0.timestamp < $1.timestamp
         }
-        if let id = self.messages.last?.id {
-            self.lastMessageID = id
+    }
+    
+    func getMyMessageHistory() {
+        let currentId = Auth.auth().currentUser!.uid
+        
+        for message in self.messages.reversed() {
+            let otherUser = message.sender == currentId ? message.recipient : message.sender
+            if !self.recentMessages.keys.contains(otherUser) {
+                self.recentMessages[otherUser] = message
+            }
         }
     }
-
+    
+    func getConversation(partnerId: String) {
+        let myId = Auth.auth().currentUser!.uid
+        
+        // Fetch all messages I have sent in this conversation.
+        let sentMessages = db.collection("Messages")
+            .whereField("sender", isEqualTo: myId)
+            .whereField("recipient", isEqualTo: partnerId)
+        getMessages(query: sentMessages)
+        
+        // Fetch all messages I have received in this conversation.
+        let receivedMessages = db.collection("Messages")
+            .whereField("sender", isEqualTo: partnerId)
+            .whereField("recipient", isEqualTo: myId)
+        getMessages(query: receivedMessages)
+        
+        // Order messages in chronological order.
+        self.messages.sort {
+            $0.timestamp < $1.timestamp
+        }
+    }
+    
     func getMessages(query: Query) {
         query.getDocuments() { querySnapshot, err in
             if let err = err {
@@ -63,17 +90,18 @@ class MessagesVM: ObservableObject {
     }
     
     func sendMessage(text: String) {
+        let myId = Auth.auth().currentUser!.uid
+        
         do {
             let newMessage = Message(
                 id: "\(UUID())",
                 text: text,
-                sender: "",
+                sender: myId,
                 recipient: "",
                 received: false,
                 timestamp: Date())
             try db.collection("Messages").document(newMessage.id).setData(from: newMessage)
-
-        } catch{
+        } catch {
             print ("Error adding message to Firestore: \(error)")
         }
     }
