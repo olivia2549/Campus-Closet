1dadff729db98ff4c8df7061eb8254f1e54c0513
hillyyehoshua <71408030+hillyyehoshua@users.noreply.github.com>
2022-11-16 19:13:41 -0600

    pushing navigation link for messages
    pushing navigation link for messages

--- Campus-Closet/Views/Messages/MainMessagesView.swift
+++ Campus-Closet/Views/Messages/MainMessagesView.swift
@@ -89,25 +89,27 @@ struct MainMessagesView: View {
         ScrollView{
             ForEach(0..<10, id: \.self){ num in
                 VStack{
-                    HStack (spacing: 16){
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 32))
-                            .padding(8)
-                            .overlay(RoundedRectangle(cornerRadius: 44)
-                                .stroke(Color.black, lineWidth: 1))
-                        
-                        
-                        VStack (alignment: .leading){
-                            Text("Username")
-                                .font(.system(size:16, weight: .bold))
-                            Text("Message sent to user")
-                                .font(.system(size:14))
-                                .foregroundColor(Color(.lightGray))
+                    NavigationLink(destination: Chat_Message()) {
+                        HStack (spacing: 16){
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 32))
+                                .padding(8)
+                                .overlay(RoundedRectangle(cornerRadius: 44)
+                                    .stroke(Color.black, lineWidth: 1))
+                            
+                            
+                            VStack (alignment: .leading){
+                                Text("Username")
+                                    .font(.system(size:16, weight: .bold))
+                                Text("Message sent to user")
+                                    .font(.system(size:14))
+                                    .foregroundColor(Color(.lightGray))
+                            }
+                            Spacer()
+                            
+                            Text("22d")
+                                .font(.system(size: 14, weight: .semibold))
                         }
-                        Spacer()
-
-                        Text("22d")
-                            .font(.system(size: 14, weight: .semibold))
                     }
                 }
                 
