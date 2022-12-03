c205a32593711dc5f09a467f7ae751ca66fe1c2d
Lauren Scott <lauren.e.scott@vanderbilt.edu>
2022-12-02 23:20:46 -0600

    Initial navigation link to seller profile page

---
+++ Campus-Closet/Views/Profile/SellerProfileView.swift
@@ -0,0 +1,206 @@
+//
+//  SellerProfileView.swift
+//  Campus-Closet
+//
+//  Created by Lauren Scott on 12/2/22.
+//
+
+import SwiftUI
+import SegmentedPicker
+
+struct SellerProfileView: View {
+    @EnvironmentObject private var viewModel: ProfileVM
+    @EnvironmentObject var session: OnboardingVM
+    @State var offset: CGFloat = 0
+    var maxHeight: CGFloat = (UIDevice.current.userInterfaceIdiom == .phone) ?
+    UIScreen.main.bounds.height / 2.6 : UIScreen.main.bounds.height / 2.0
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 10) {
+                GeometryReader{ proxy in
+                    SellerProfileInfo(offset: $offset, proxy: proxy)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: getHeaderHeight(proxy: proxy), alignment: .bottom)
+                        .overlay(
+                            SellerProfileHeader(
+                                offset: $offset,
+                                proxy: proxy
+                            )
+                            ,alignment: .top
+                        )
+                }
+                .frame(height: maxHeight)
+                .offset(y: -offset)
+                .zIndex(1)
+                
+                SellerToggleView(maxHeight: maxHeight, tabs: ["Listings (\(viewModel.user.listings.count))", "Sold (\(viewModel.user.sold.count))"])
+                    .zIndex(0)
+            }
+            .modifier(OffsetModifier(offset: $offset))
+        }
+        .environmentObject(viewModel)
+        .environmentObject(session)
+        .coordinateSpace(name: "SCROLL")
+        .navigationBarHidden(true)
+    }
+    
+    func getHeaderHeight(proxy: GeometryProxy) -> CGFloat {
+        let topHeight = maxHeight + offset
+        return topHeight > proxy.size.height*0.15 ? topHeight : proxy.size.height*0.15
+    }
+    
+}
+
+struct SellerProfileHeader: View {
+    @EnvironmentObject private var viewModel: ProfileVM
+    @Binding var offset: CGFloat
+    var proxy: GeometryProxy
+    
+    var body: some View {
+        HStack {
+            Text(viewModel.user.name)
+                .font(.system(size: 20, weight: .semibold))
+                .frame(maxWidth: .infinity)
+                .opacity(getNameOpacity(proxy: proxy))
+            VStack(alignment: .center) {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: proxy.size.width * 0.3)
+            }
+            .opacity(getOpacity(proxy: proxy))
+            .frame(maxWidth: .infinity)
+            VStack(alignment: .trailing) {}
+            .frame(maxWidth: .infinity)
+        }
+        .ignoresSafeArea()
+        .frame(height: proxy.size.height * 0.15)
+        .frame(maxWidth: .infinity)
+        .background(.white)
+    }
+    
+    func getOpacity(proxy: GeometryProxy) -> CGFloat {
+        let progress = -offset/(maxHeight*0.1)
+        let opacity = 1-progress
+        return offset < 0 ? opacity : 1
+    }
+    
+    func getNameOpacity(proxy: GeometryProxy) -> CGFloat {
+        let progress = -offset/(maxHeight*0.6)
+        return progress
+    }
+    
+}
+
+struct SellerProfileInfo: View {
+    @EnvironmentObject private var viewModel: ProfileVM
+    @EnvironmentObject var session: OnboardingVM
+    @Binding var offset: CGFloat
+    var proxy: GeometryProxy
+    
+    var body: some View {
+        VStack {
+            if (viewModel.profilePicture != nil) {
+                Image(uiImage: viewModel.profilePicture!)
+                    .resizable()
+                    .frame(width: proxy.size.width*0.4, height: proxy.size.width*0.4, alignment: .center)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            }
+            else {
+                Image("blank-profile")
+                    .resizable()
+                    .frame(width: proxy.size.width*0.4, height: proxy.size.width*0.4, alignment: .center)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            }
+            Text(viewModel.user.name)
+                .font(.system(size: 24, weight: .medium))
+            HStack(alignment: .center) {
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: proxy.size.width*0.06)
+                    .foregroundColor(Styles().themePink)
+                Text(String(format: "%.2f", viewModel.averageRating))
+                Text("(\(viewModel.numRatings) \(viewModel.numRatings == 1 ? "Rating" : "Ratings"))")
+                Text("|")
+                Image(systemName: "dollarsign.circle")
+                    .foregroundColor(Styles().themePink)
+                Text(viewModel.user.venmo)
+            }
+        }
+        .ignoresSafeArea()
+        .frame(maxWidth: .infinity)
+        .opacity(getOpacity())
+        .background(.white)
+    }
+    
+    func getOpacity() -> CGFloat {
+        let progress = -offset/90
+        let opacity = 1-progress
+        return offset < 0 ? opacity : 1
+    }
+}
+
+struct SellerToggleView: View {
+    @EnvironmentObject var viewModel: ProfileVM
+    @EnvironmentObject var session: OnboardingVM
+    @State var offset: CGFloat = 0
+    var maxHeight: CGFloat
+    var tabs: [String]
+    var selection: Binding<Data.Index?> {
+        Binding(
+            get: { viewModel.position.rawValue },
+            set: {
+                if session.isLoggedIn {
+                    viewModel.getProfileData()
+                }
+                viewModel.position = Position(rawValue: $0!) ?? Position.first
+            }
+        )
+    }
+    
+    var body: some View {
+        VStack {
+            SegmentedPicker(
+                tabs,
+                selectedIndex: selection,
+                selectionAlignment: .bottom,
+                content: { item, isSelected in
+                    HStack {
+                        VStack(spacing: 0) {
+                            Text(item)
+                                .foregroundColor(isSelected ? Styles().themePink : Color.gray )
+                                .font(.system(size: 20, weight: .semibold))
+                                .padding(.vertical, 8)
+                            if !isSelected {
+                                Color("Search Bar").frame(height: 1)
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                },
+                selection: {
+                    HStack {
+                        VStack(spacing: 0) {
+                            Spacer()
+                            Styles().themePink.frame(height: 1)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                })
+                .animation(.easeInOut(duration: 0.3))
+            Masonry<ProfileVM>()
+                .zIndex(0)
+        }
+        .environmentObject(session)
+        .environmentObject(viewModel)
+    }
+    
+    func getPosition() -> CGFloat {
+        return offset < maxHeight ? (80 - offset) : (maxHeight - offset)
+    }
+    
+}
