add455f469678e24e3e0b81b635401eab3bc7ab8
arpeppard <amanda.r.peppard@vanderbilt.edu>
2022-12-06 03:04:47 -0600

    post UI test updates

--- Campus-ClosetUITests/PostUITests.swift
+++ Campus-ClosetUITests/PostUITests.swift
@@ -35,7 +35,7 @@ class PostUITests: XCTestCase {
         XCTAssertTrue(createPostButton.exists)
         createPostButton.tap()
         
-        let choosePicButton = app.buttons["Choose a Picture"]
+        let choosePicButton = app.buttons["Choose a picture"]
         XCTAssertTrue(choosePicButton.exists)
         choosePicButton.tap()
         let testImage = app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Photo, March 30, 2018, 2:14 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Photo, March 30, 2018, 2:14 PM, Photo, August 08, 2012, 4:55 PM, Photo, August 08, 2012, 4:29 PM, Photo, August 08, 2012, 1:52 PM, Photo, October 09, 2009, 4:09 PM, Photo, March 12, 2011, 6:17 PM\"].images[\"Photo, March 30, 2018, 2:14 PM\"]",".images[\"Photo, March 30, 2018, 2:14 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
@@ -69,16 +69,18 @@ class PostUITests: XCTestCase {
         let sizeText = element.children(matching: .textField).element(boundBy: 2)
         sizeText.typeText("S")
         
-        let conditionField = app.staticTexts["Condition*"]
+        let conditionField = app.buttons["Condition*"]
         XCTAssertTrue(conditionField.exists)
         conditionField.tap()
-        let conditionText = element.children(matching: .textField).element(boundBy: 3)
-        conditionText.typeText("Lightly used")
+        let newOption = app.collectionViews.buttons["New"]
+        XCTAssertTrue(newOption.exists)
+        newOption.tap()
         
         let description = app.staticTexts["Description"]
         XCTAssertTrue(description.exists)
         description.tap()
-        let descriptionText = element.children(matching: .textField).element(boundBy: 4)
+        let descriptionText = element.children(matching: .textField).element(boundBy: 3)
+        descriptionText.tap()
         descriptionText.typeText("Test description")
         
         app.buttons["Done"].tap()
@@ -88,11 +90,25 @@ class PostUITests: XCTestCase {
         XCTAssertTrue(postItemButton.exists)
         postItemButton.tap()
    
-        //right now the "post item" button does not do anything
-        //finish implementation once bug is fixed
-     
-             
+        //assert that once item is posted, takes you back to profile
+        let elementsQuery = app.scrollViews.otherElements
+        //asserting these symbols exist indicate that the UI flow has navigated to profile page
+        let venmoSign = elementsQuery.images["dollarsign.circle"]
+        XCTAssertTrue(venmoSign.exists)
+        let buyerButton = elementsQuery.segmentedControls.containing(.button, identifier:"Buyer").element
+        XCTAssertTrue(buyerButton.exists)
+        let sellerButton = elementsQuery.segmentedControls.containing(.button, identifier:"Seller").element
+        sellerButton.tap()
         
+        //tapping first item in listings
+        app.scrollViews.otherElements.containing(.image, identifier:"logo").children(matching: .other).element(boundBy: 0).children(matching: .button).element(boundBy: 0).tap()
+        
+        //asserts that item that has been tapped is the item just posted
+        //by asserting descriptive characteristics are the same
+        XCTAssertTrue(app.staticTexts["Test Item"].exists)
+        XCTAssertTrue(elementsQuery.staticTexts["Test description"].exists)
+        XCTAssertTrue(elementsQuery.staticTexts["New"].exists)
+                    
     }
 
     func testPriceError() throws {
@@ -104,7 +120,7 @@ class PostUITests: XCTestCase {
         XCTAssertTrue(createPostButton.exists)
         createPostButton.tap()
         
-        let choosePicButton = app.buttons["Choose a Picture"]
+        let choosePicButton = app.buttons["Choose a picture"]
         XCTAssertTrue(choosePicButton.exists)
         choosePicButton.tap()
         let testImage = app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Photo, March 30, 2018, 2:14 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Photo, March 30, 2018, 2:14 PM, Photo, August 08, 2012, 4:55 PM, Photo, August 08, 2012, 4:29 PM, Photo, August 08, 2012, 1:52 PM, Photo, October 09, 2009, 4:09 PM, Photo, March 12, 2011, 6:17 PM\"].images[\"Photo, March 30, 2018, 2:14 PM\"]",".images[\"Photo, March 30, 2018, 2:14 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
@@ -124,7 +140,7 @@ class PostUITests: XCTestCase {
 
         let itemNameText = element.children(matching: .textField).element(boundBy: 0)
         itemNameText.tap()
-        itemNameText.typeText("Test Item Price Error")
+        itemNameText.typeText("Test Item Pricing Error")
         
         let priceField = app.staticTexts["Price*"]
         XCTAssertTrue(priceField.exists)
@@ -136,19 +152,21 @@ class PostUITests: XCTestCase {
         XCTAssertTrue(sizeField.exists)
         sizeField.tap()
         let sizeText = element.children(matching: .textField).element(boundBy: 2)
-        sizeText.typeText("M")
+        sizeText.typeText("S")
         
-        let conditionField = app.staticTexts["Condition*"]
+        let conditionField = app.buttons["Condition*"]
         XCTAssertTrue(conditionField.exists)
         conditionField.tap()
-        let conditionText = element.children(matching: .textField).element(boundBy: 3)
-        conditionText.typeText("Used")
+        let newOption = app.collectionViews.buttons["New"]
+        XCTAssertTrue(newOption.exists)
+        newOption.tap()
         
         let description = app.staticTexts["Description"]
         XCTAssertTrue(description.exists)
         description.tap()
-        let descriptionText = element.children(matching: .textField).element(boundBy: 4)
-        descriptionText.typeText("Test description price error")
+        let descriptionText = element.children(matching: .textField).element(boundBy: 3)
+        descriptionText.tap()
+        descriptionText.typeText("Test pricing error")
         
         app.buttons["Done"].tap()
         element.buttons["Next"].tap()
@@ -174,7 +192,7 @@ class PostUITests: XCTestCase {
         XCTAssertTrue(createPostButton.exists)
         createPostButton.tap()
         
-        let choosePicButton = app.buttons["Choose a Picture"]
+        let choosePicButton = app.buttons["Choose a picture"]
         XCTAssertTrue(choosePicButton.exists)
         choosePicButton.tap()
         let testImage = app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Photo, March 30, 2018, 2:14 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Photo, March 30, 2018, 2:14 PM, Photo, August 08, 2012, 4:55 PM, Photo, August 08, 2012, 4:29 PM, Photo, August 08, 2012, 1:52 PM, Photo, October 09, 2009, 4:09 PM, Photo, March 12, 2011, 6:17 PM\"].images[\"Photo, March 30, 2018, 2:14 PM\"]",".images[\"Photo, March 30, 2018, 2:14 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
@@ -217,14 +235,27 @@ class PostUITests: XCTestCase {
                 
     }
     
-    
-    func cancelPost() throws {
+    func testCancelPost() throws {
         app.images["Add To Home Screen"].tap()
-        app.buttons["Choose a Picture"].tap()
+        app.buttons["Choose a picture"].tap()
         
         app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Photo, October 09, 2009, 4:09 PM"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Photo, March 30, 2018, 2:14 PM, Photo, August 08, 2012, 4:55 PM, Photo, August 08, 2012, 4:29 PM, Photo, August 08, 2012, 1:52 PM, Photo, October 09, 2009, 4:09 PM, Photo, March 12, 2011, 6:17 PM\"].images[\"Photo, October 09, 2009, 4:09 PM\"]",".images[\"Photo, October 09, 2009, 4:09 PM\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
         app.buttons["Next"].tap()
-        app.navigationBars["_TtGC7SwiftUI19UIHosting"]/*@START_MENU_TOKEN@*/.buttons["Back"]/*[[".otherElements[\"Back\"].buttons[\"Back\"]",".buttons[\"Back\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        
+        let itemNameField = app.staticTexts["Item Name*"]
+        XCTAssertTrue(itemNameField.exists)
+        itemNameField.tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        
+
+        let itemNameText = element.children(matching: .textField).element(boundBy: 0)
+        itemNameText.tap()
+        itemNameText.typeText("Test Cancel Post")
+        
+        
+        app.navigationBars["_TtGC7SwiftUI32NavigationStackHosting"]/*@START_MENU_TOKEN@*/.buttons["Back"]/*[[".otherElements[\"Back\"].buttons[\"Back\"]",".buttons[\"Back\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
         let homeButton = app.tabBars["Tab Bar"].buttons["Home"]
         XCTAssertTrue(homeButton.exists)
@@ -234,24 +265,16 @@ class PostUITests: XCTestCase {
         profileButton.tap()
         
         let elementsQuery = app.scrollViews.otherElements
-        let sellerTab = elementsQuery/*@START_MENU_TOKEN@*/.buttons["Seller"]/*[[".segmentedControls.buttons[\"Seller\"]",".buttons[\"Seller\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        XCTAssertTrue(sellerTab.exists)
-        sellerTab.tap()
+        let sellerButton = elementsQuery.segmentedControls.containing(.button, identifier:"Seller").element
+        sellerButton.tap()
         
-        // also click listings
-        let nothingListed = elementsQuery.staticTexts["Nothing to show"]
-        XCTAssertTrue(nothingListed.exists)
+        //tapping first item in listings
+        app.scrollViews.otherElements.containing(.image, identifier:"logo").children(matching: .other).element(boundBy: 0).children(matching: .button).element(boundBy: 0).tap()
         
-    }
+        //asserts that item that has been canceled does not exist as most recent item
+        XCTAssertFalse(app.staticTexts["Test Cancel Post"].exists)
+      
     
-    
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
 
